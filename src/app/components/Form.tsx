8763156f2d2be981addac6f824d5b29999264830
Suka <mateodefalco1@gmail.com>
2023-08-07 15:39:55 -0300

    Updates form styles

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -12,9 +12,9 @@ function Form({
 	return (
 		<form className="w-full">
 			<label className="block space-y-3 text-lg">
-				<span>Monto en ARS:</span>
+				<span className="text-black">Monto en ARS:</span>
 				<input
-					className="block w-full rounded-full bg-gray-200 p-2 text-right text-3xl"
+					className="block w-full rounded-full bg-gray-300 p-2 text-right text-3xl"
 					type="number"
 					value={value}
 					onChange={(e) => onChange(Number(e.target.value))}
